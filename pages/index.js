7b2427b582fa5e0a890a44bcfb99bc6a35ba2959
xinmeng2020 <xinmengli2020@gmail.com>
2021-02-26 16:56:41 +0100

    update3

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Xinmeng Li's Update</title>
-	      <link rel="icon" href="/favicon.ico" />
+	      <link rel="icon" href="/favicon2.ico" />
       </Head>
       
 
